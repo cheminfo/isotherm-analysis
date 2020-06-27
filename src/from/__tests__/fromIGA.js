0a4aa139737cda2a67fd0c3268ea9aac5e6eb044
Kevin <kevin.jablonka@epfl.ch>
2020-06-27 12:04:09 +0200

    feat: first draft of IGA. Have to discuss with Chris if this fits the needs?

--- src/from/__tests__/fromIGA.js
+++ src/from/__tests__/fromIGA.js
@@ -7,7 +7,8 @@ import { exception } from 'console';
 const {
   getLineNumbersOfMeasurement,
   parseIGAMeasurmentHeader,
-  parseDataBlock,
+  parseIGADataBlock,
+  parseOneIGA,
 } = testables;
 
 test('fromPerkinElmer', () => {
@@ -21,16 +22,22 @@ test('fromPerkinElmer', () => {
   const linenumbers = getLineNumbersOfMeasurement(lines);
   expect(linenumbers).toStrictEqual([
     [1, 107, 213, 319],
-    [104, 210, 316, 422],
+    [106, 212, 318, 424],
   ]);
 
   const header = parseIGAMeasurmentHeader(lines.slice(3, 69));
-  expect(header.Scan).toStrictEqual(1);
-  expect(header.SampleWeight).toStrictEqual(9.2627);
-  expect(header.SampleWeightDry).toStrictEqual(8.9674);
+  expect(header.scan).toStrictEqual(1);
+  expect(header.sampleWeight).toStrictEqual(9.2627);
+  expect(header.sampleWeightDry).toStrictEqual(8.9674);
 
-  const dataBlock = parseDataBlock(lines.slice(74, 103));
-  console.log(dataBlock);
+  const dataBlock = parseIGADataBlock(lines.slice(73, 103));
+  expect(dataBlock.pressure[0]).toStrictEqual(-0.7);
+  expect(dataBlock.gasDensity[0]).toStrictEqual(0.0);
+
+  const meas = parseOneIGA(lines.slice(1, 106));
+  expect(meas.meta.sampleNumber).toStrictEqual('0328');
+  expect(meas.data.wtPercent).toHaveLength(30);
+  expect(meas.data.wtPercent[29]).toStrictEqual(1.447);
   // let analysis = fromIGA(file);
 
   // let spectrum1 = analysis.getSpectrum();
--- src/from/fromIGA.js
+++ src/from/fromIGA.js
@@ -7,82 +7,82 @@ function lineSplitTrim(line) {
 function parseIGAMeasurmentHeader(lines) {
   let metaData = {};
   // Let's use some cleaner names
-  metaData.SystemType = lineSplitTrim(lines[0]);
-  metaData.SystemOwner = lineSplitTrim(lines[1]);
-  metaData.SystemReference = lineSplitTrim(lines[2]);
-  metaData.SystemSerialNumber = lineSplitTrim(lines[3]);
-  metaData.SampleNumber = lineSplitTrim(lines[4]);
-  metaData.ExperimentType = lineSplitTrim(lines[5]);
-  metaData.ExperimentNumber = parseInt(lineSplitTrim(lines[6]));
-  metaData.Title = lineSplitTrim(lines[7]);
-  metaData.Comment = lineSplitTrim(lines[8]);
-  metaData.Source = lineSplitTrim(lines[9]);
-  metaData.Batch = lineSplitTrim(lines[10]);
-  metaData.ID = lineSplitTrim(lines[11]);
-  metaData.ExperimentTitle = lineSplitTrim(lines[12]);
+  metaData.systemType = lineSplitTrim(lines[0]);
+  metaData.systemOwner = lineSplitTrim(lines[1]);
+  metaData.systemReference = lineSplitTrim(lines[2]);
+  metaData.systemSerialNumber = lineSplitTrim(lines[3]);
+  metaData.sampleNumber = lineSplitTrim(lines[4]);
+  metaData.experimentType = lineSplitTrim(lines[5]);
+  metaData.experimentNumber = parseInt(lineSplitTrim(lines[6]));
+  metaData.title = lineSplitTrim(lines[7]);
+  metaData.comment = lineSplitTrim(lines[8]);
+  metaData.source = lineSplitTrim(lines[9]);
+  metaData.batch = lineSplitTrim(lines[10]);
+  metaData.id = lineSplitTrim(lines[11]);
+  metaData.experimentTitle = lineSplitTrim(lines[12]);
   let tmp = lineSplitTrim(lines[14]).split(/\s/);
-  metaData.SampleWeight = parseFloat(tmp[0]);
-  metaData.SampleWeightUnit = tmp[1];
+  metaData.sampleWeight = parseFloat(tmp[0]);
+  metaData.sampleWeightUnit = tmp[1];
 
   tmp = lineSplitTrim(lines[15]).split(/\s/);
-  metaData.SampleWeightDry = parseFloat(tmp[0]);
-  metaData.SampleWeightDryUnit = tmp[1];
+  metaData.sampleWeightDry = parseFloat(tmp[0]);
+  metaData.sampleWeightDryUnit = tmp[1];
 
   tmp = lineSplitTrim(lines[18]).split(/\s/);
-  metaData.BalanceTrimV = parseFloat(tmp[0]);
-  metaData.BalanceTrimVUnit = tmp[1];
+  metaData.balanceTrimV = parseFloat(tmp[0]);
+  metaData.balanceTrimVUnit = tmp[1];
 
-  metaData.BalanceTrimT = lineSplitTrim(lines[19]);
+  metaData.balanceTrimT = lineSplitTrim(lines[19]);
 
   tmp = lineSplitTrim(lines[20]).split(/\s/);
-  metaData.CounterWeightM = parseFloat(tmp[0]);
-  metaData.CounterWeightMUnit = tmp[1];
+  metaData.counterWeightM = parseFloat(tmp[0]);
+  metaData.counterWeightMUnit = tmp[1];
 
   tmp = lineSplitTrim(lines[21]).split(/\s/);
-  metaData.CounterWeightRho = parseFloat(tmp[0]);
-  metaData.CounterWeightRhoUnit = tmp[1];
+  metaData.counterWeightRho = parseFloat(tmp[0]);
+  metaData.counterWeightRhoUnit = tmp[1];
 
-  metaData.CounterWeightT = lineSplitTrim(lines[22]);
+  metaData.counterWeightT = lineSplitTrim(lines[22]);
 
   tmp = lineSplitTrim(lines[23]).split(/\s/);
-  metaData.TungstenM = parseFloat(tmp[0]);
-  metaData.TungstenMUnit = tmp[1];
+  metaData.tungstenM = parseFloat(tmp[0]);
+  metaData.tungstenMUnit = tmp[1];
 
   tmp = lineSplitTrim(lines[24]).split(/\s/);
-  metaData.TungstenRho = parseFloat(tmp[0]);
-  metaData.TungstenRhoUnit = tmp[1];
+  metaData.tungstenRho = parseFloat(tmp[0]);
+  metaData.tungstenRhoUnit = tmp[1];
 
-  metaData.CounterWeightT = lineSplitTrim(lines[25]);
+  metaData.counterWeightT = lineSplitTrim(lines[25]);
 
   tmp = lineSplitTrim(lines[26]).split(/\s/);
-  metaData.ChainExcessM = parseFloat(tmp[0]);
-  metaData.ChainExcessMUnit = tmp[1];
+  metaData.chainExcessM = parseFloat(tmp[0]);
+  metaData.chainExcessMUnit = tmp[1];
 
   tmp = lineSplitTrim(lines[27]).split(/\s/);
-  metaData.ChainExcessRho = parseFloat(tmp[0]);
-  metaData.ChainExcessRhoUnit = tmp[1];
+  metaData.chainExcessRho = parseFloat(tmp[0]);
+  metaData.chainExcessRhoUnit = tmp[1];
 
-  metaData.ChainExcessT = lineSplitTrim(lines[28]);
+  metaData.chainExcessT = lineSplitTrim(lines[28]);
 
-  metaData.ApplicationCode = lineSplitTrim(lines[44]);
-  metaData.Mode = lineSplitTrim(lines[44]);
-  metaData.Source = lineSplitTrim(lines[45]);
-  metaData.GasSource = lineSplitTrim(lines[46]);
-  metaData.Vessel = lineSplitTrim(lines[47]);
-  metaData.Reactor = lineSplitTrim(lines[48]);
-  metaData.PressureSensor = lineSplitTrim(lines[49]);
-  metaData.Thermostat = lineSplitTrim(lines[50]);
+  metaData.applicationCode = lineSplitTrim(lines[44]);
+  metaData.mode = lineSplitTrim(lines[44]);
+  metaData.source = lineSplitTrim(lines[45]);
+  metaData.gasSource = lineSplitTrim(lines[46]);
+  metaData.vessel = lineSplitTrim(lines[47]);
+  metaData.reactor = lineSplitTrim(lines[48]);
+  metaData.pressureSensor = lineSplitTrim(lines[49]);
+  metaData.thermostat = lineSplitTrim(lines[50]);
 
-  metaData.NitrogenEOS = lineSplitTrim(lines[51]);
-  metaData.NitrogenSVP = lineSplitTrim(lines[52]);
+  metaData.nitrogenEOS = lineSplitTrim(lines[51]);
+  metaData.nitrogenSVP = lineSplitTrim(lines[52]);
 
-  metaData.SeriesType = lineSplitTrim(lines[59]);
+  metaData.seriesType = lineSplitTrim(lines[59]);
 
-  metaData.Scan = parseInt(lineSplitTrim(lines[61]));
-  metaData.Course = lineSplitTrim(lines[62]);
-  metaData.ReferenceStateDevice = lineSplitTrim(lines[63]);
-  metaData.ScanTitle = lineSplitTrim(lines[64]);
-  metaData.ScanStart = lineSplitTrim(lines[65]);
+  metaData.scan = parseInt(lineSplitTrim(lines[61]));
+  metaData.course = lineSplitTrim(lines[62]);
+  metaData.referenceStateDevice = lineSplitTrim(lines[63]);
+  metaData.scanTitle = lineSplitTrim(lines[64]);
+  metaData.scanStart = lineSplitTrim(lines[65]);
 
   return metaData;
 }
@@ -94,19 +94,19 @@ function getLineNumbersOfMeasurement(lines) {
     if (line.match('S E R I E S   D A T A   R E C O R D')) {
       starts.push(index);
     } else if (line.match('Scan Ends ')) {
-      ends.push(index);
+      ends.push(index + 2);
     }
   }
   return [starts, ends];
 }
 
-function parseDataBlock(lines) {
+function parseIGADataBlock(lines) {
   let dataBlock = {
     pressure: [],
     gasDensity: [],
     pp0: [],
     totalWeight: [],
-    BET: [],
+    bet: [],
     excessAdsorptionPercentage: [],
     excessAdsorption: [],
     sampleT: [],
@@ -121,13 +121,21 @@ function parseDataBlock(lines) {
     dataBlock.totalWeight.push(parseFloat(tmp[3]));
     dataBlock.excessAdsorptionPercentage.push(parseFloat(tmp[4]));
     dataBlock.excessAdsorption.push(parseFloat(tmp[5]));
-    dataBlock.BET.push(parseFloat(tmp[6]));
+    dataBlock.bet.push(parseFloat(tmp[6]));
     dataBlock.sampleT.push(parseFloat(tmp[7]));
     dataBlock.wtPercent.push(parseFloat(tmp[8]));
   }
   return dataBlock;
 }
 
+function parseOneIGA(lines) {
+  const measLen = lines.length;
+  let meta = parseIGAMeasurmentHeader(lines.slice(2, 68));
+  const dataBlock = parseIGADataBlock(lines.slice(72, measLen - 3));
+  meta.scanEnd = lineSplitTrim(lines[measLen - 2]);
+  meta.scanReferenceState = lineSplitTrim(lines[measLen - 1]);
+  return { meta: meta, data: dataBlock };
+}
 /**
  * Creates a new Analysis element
  * @param {string} text - String containing the IGA analysis data, maybe contain multiple measurements
@@ -136,46 +144,76 @@ function parseDataBlock(lines) {
 export function fromIGA(text) {
   const lines = text.split(/[\r\n]+/);
 
-  //     let parsed = Papa.parse(text, {
-  //     skipEmptyLines: true,
-  //     header: true,
-  //     dynamicTyping: true,
-  //   }).data;
+  let analyses = [];
+  const lineNumbers = getLineNumbersOfMeasurement(lines);
+  for (let lineNumber of lineNumbers) {
+    let meas = parseOneIGA(lines.slice(lineNumber[0], lineNumber[1]));
 
-  //   let analysis = new Analysis();
-  //   analysis.pushSpectrum(
-  //     {
-  //       x: {
-  //         data: parsed.map((d) => d['Program Temperature']),
-  //         label: 'Program temperature [°C]',
-  //       },
-  //       y: {
-  //         data: parsed.map((d) => d['Unsubtracted Weight']),
-  //         label: 'Weight [mg]',
-  //       },
-  //       t: {
-  //         data: parsed.map((d) => d['Sample Temperature']),
-  //         label: 'Sample temperature [°C]',
-  //       },
-  //     },
-  //     { dataType: 'TGA' },
-  //   );
+    let analysis = new Analysis();
 
-  //   analysis.pushSpectrum(
-  //     {
-  //       x: { data: parsed.map((d) => d.Time), label: 'Time [s]' },
-  //       y: {
-  //         data: parsed.map((d) => d['Unsubtracted Weight']),
-  //         label: 'Weight [mg]',
-  //       },
-  //     },
-  //     { dataType: 'TGA' },
-  //   );
-  return analysis;
+    analysis.pushSpectrum(
+      {
+        x: {
+          data: meas.data.pressure,
+          label: 'Pressure  [mbar]',
+        },
+        y: {
+          data: meas.data.excessAdsorption,
+          label: 'Excess Adsorption [mmol/g]',
+        },
+      },
+      {
+        dataType: 'Isotherm',
+        title: meas.meta.experimentTitle,
+        meta: meas.meta,
+      },
+    );
+
+    analysis.pushSpectrum(
+      {
+        x: {
+          data: meas.data.pressure,
+          label: 'Pressure  [mbar]',
+        },
+        y: {
+          data: meas.data.excessAdsorptionPercentage,
+          label: 'Excess Adsorption [%]',
+        },
+      },
+      {
+        dataType: 'Isotherm',
+        title: meas.meta.experimentTitle,
+        meta: meas.meta,
+      },
+    );
+
+    analysis.pushSpectrum(
+      {
+        x: {
+          data: meas.data.pp0,
+          label: 'relative pressure',
+        },
+        y: {
+          data: meas.data.excessAdsorption,
+          label: 'Excess Adsorption [mmol/g]',
+        },
+      },
+      {
+        dataType: 'Isotherm',
+        title: meas.meta.experimentTitle,
+        meta: meas.meta,
+      },
+    );
+
+    analyses.push(analysis);
+  }
+
+  return analyses;
 }
 
 export const testables = {
   getLineNumbersOfMeasurement: getLineNumbersOfMeasurement,
   parseIGAMeasurmentHeader: parseIGAMeasurmentHeader,
-  parseDataBlock: parseDataBlock,
+  parseIGADataBlock: parseIGADataBlock,
+  parseOneIGA: parseOneIGA,
 };
