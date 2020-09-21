e6fe5a9135595399d10ac81e34ab1f14e275f1a8
Kevin <kevin.jablonka@epfl.ch>
2020-09-21 13:28:46 +0200

    chore: lint

--- src/from/fromBelsorp.js
+++ src/from/fromBelsorp.js
@@ -130,17 +130,17 @@ export function fromBelsorp(path) {
       x: {
         data: data.adsorption.pp0,
         label: 'relative pressure',
-        type: 'independent' 
+        type: 'independent',
       },
       p: {
         data: data.adsorption.pe,
         label: 'Pressure  [kPa]',
-        type: 'independent' 
+        type: 'independent',
       },
       y: {
         data: data.adsorption.va,
         label: 'Excess adsorption mmol /g',
-        type: 'dependent' 
+        type: 'dependent',
       },
     },
     {
