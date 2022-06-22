379c93fe96d8e3b5c506bbbf503e5f6b799dec63
Luc Patiny <luc@patiny.com>
2022-06-22 16:56:50 +0200

    test: fix tests

--- src/from/__tests__/fromBelsorpCSV.test.js
+++ src/from/__tests__/fromBelsorpCSV.test.js
@@ -4,19 +4,23 @@ import { join } from 'path';
 import { fromBelsorpCSV } from '../fromBelsorpCSV';
 
 describe('fromBelsorpCSV', () => {
-  it('belsorp.csv', () => {
+  it.only('belsorp.csv', () => {
     const binary = readFileSync(join(__dirname, 'data/belsorp.csv'));
     const analysis = fromBelsorpCSV(binary);
+    delete analysis.spectra[0].id;
+    delete analysis.spectra[1].id;
     expect(analysis.spectra).toMatchSnapshot();
   });
   it('belsorp2.csv', () => {
     const binary = readFileSync(join(__dirname, 'data/belsorp2.csv'));
     const analysis = fromBelsorpCSV(binary);
+    delete analysis.spectra[0].id;
     expect(analysis.spectra).toMatchSnapshot();
   });
   it('belsorp3.csv', () => {
     const binary = readFileSync(join(__dirname, 'data/belsorp3.csv'));
     const analysis = fromBelsorpCSV(binary);
+    delete analysis.spectra[0].id;
     expect(analysis.spectra).toMatchSnapshot();
   });
 });
